0ef1621f15df221d1bdceebdb9b1b37af958c34f
dinwwwh <dinwwwh@gmail.com>
2024-08-15 08:15:06 +0700

    chore: example markdown for initial value

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,20 @@
-import type { TElement } from '@udecode/plate-common'
+import { type TElement, createPlateEditor, deserializeHtml } from '@udecode/plate-common'
 import { useEffect, useState } from 'react'
+import { marked } from 'marked'
 import { container } from './app.css'
+import { plugins } from './plate/plugins'
 import { Editor } from '.'
 
+const markdown = `
+paragraph: lorem ipsum
+`
+
+const editor = createPlateEditor({ plugins })
+const html = await marked.parse(markdown)
+const initialValue = deserializeHtml(editor, { element: html }) as TElement[]
+
 export function App() {
-  const [value, setValue] = useState<TElement[] | undefined>(undefined)
+  const [value, setValue] = useState<TElement[] | undefined>()
 
   useEffect(() => {
     // eslint-disable-next-line no-console
@@ -13,7 +23,7 @@ export function App() {
 
   return (
     <div className={container}>
-      <Editor initialValue={value} onChange={setValue} />
+      <Editor initialValue={initialValue} onChange={setValue} />
     </div>
   )
 }
